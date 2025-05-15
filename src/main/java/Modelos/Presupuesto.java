c8282cb4d8dbb511aab4d337d2815ecc76a2c31f
Detmermedina <demer.medina@educa.madrid.org>
2025-05-15 18:21:09 +0200

    RenamePresupuesto

--- src/main/java/Modelos/Presupuesto.java
+++ src/main/java/Modelos/Presupuesto.java
@@ -1,6 +1,15 @@
 package Modelos;
 
 public class Presupuesto {
+    public int id;
+    public String nombre;
+    public int cantidad;
+    public double precio;
 
-
+    public Presupuesto(int id, String nombre, int cantidad, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
 }
