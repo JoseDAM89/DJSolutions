8f246fc8539274474a87fb93a329f13fd3e6473b
Detmermedina <demer.medina@educa.madrid.org>
2025-06-08 20:14:04 +0200

    deja meter DNI,Pasaporte y CIF

--- src/main/java/gui/FormularioGenericoAlta.java
+++ src/main/java/gui/FormularioGenericoAlta.java
@@ -97,7 +97,11 @@ public class FormularioGenericoAlta extends JPanel {
                         @Override
                         public boolean verify(JComponent input) {
                             String texto = ((JTextField) input).getText().trim();
-                            return texto.matches("^[A-Za-z]{1}\\d{8}$");
+                            return texto.matches("^[A-Z]\\d{7}[A-Z0-9]$" // CIF
+                                    + "|^\\d{8}[A-Z]$"                         // DNI
+                                    + "|^[A-Z]{1,3}\\d{6,9}$");                // Pasaporte
+
+
                         }
 
                         @Override
